64e275b1451635ea027d076683e9967b5588057a
Anshul1507 <ag1507anshul@gmail.com>
2022-02-13 17:16:59 +0530

    Init codebase

---
+++ app/src/main/java/com/github/Anshul1507/discountnotificationfcm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.Anshul1507.discountnotificationfcm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
